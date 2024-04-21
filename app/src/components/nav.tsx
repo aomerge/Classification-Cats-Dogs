11aad2a736fcc774a00470798c1dcd4aff4d57d3
Ángel Ortega <codenode.ortega@gmail.com>
2024-04-21 03:25:12 -0600

    Add Nav component to app/src/components/nav.tsx

---
+++ app/src/components/nav.tsx
@@ -0,0 +1,13 @@
+export default function Nav (){
+    return(
+        <nav className="bg-dark text-secondary p-4 text-center">
+            <h1 className="display-5 fw-bold text-white">Ringa Tech</h1>
+            <div className="col-lg-6 mx-auto">
+                <p className="fs-5 mb-4">
+                    Revisa <a href="https://youtube.com/RingaTech">mi canal</a> para
+                    videos de inteligencia artificial, programaci&oacute;n, etc.
+                </p>
+            </div>
+        </nav>
+    );
+}
