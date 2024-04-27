d9f8947d1b4d1e24913fe698cb1a333de1e8eca7
Ángel Ortega <codenode.ortega@gmail.com>
2024-04-26 21:31:01 -0600

    Fix bug in Nav component to include REACT_APP_BASE_URL environment variable in href

--- app/src/components/nav.tsx
+++ app/src/components/nav.tsx
@@ -2,7 +2,12 @@ export default function Nav (){
     return (
       <nav className=" shadow-gray-700 shadow-xl drop-shadow-md   flex justify-around text-secondary px-14 text-center border w-1/2 py-5 rounded-full">
         <div>
-          <a href="/" className="text-white hover:text-yellow-500 text-xl">
+          <a href={
+            process.env.REACT_APP_BASE_URL
+              ? `${process.env.REACT_APP_BASE_URL}/`
+              : "/"
+          
+          } className="text-white hover:text-yellow-500 text-xl">
             Image Classification
           </a>
         </div>
